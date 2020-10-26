5866af702733f41c8057e2a5d760ecc24003458a
Yiru-SHA <68270680+Yiru-SHA@users.noreply.github.com>
2020-10-27 00:13:25 +0800

    1.0

---
+++ index.js
@@ -0,0 +1,65 @@
+//console.log('this is working!')
+
+let express = require('express');
+let app = express();
+
+// step 6 在数据返还时，只解析parses Content-Type header 符合的 type option; 让他理解req.body的数据
+// body = 储存json file的string主体
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+//install in terminal npm install body-parser --save
+
+
+// step 7 Database initialization
+// data don't know each other, no relationship database. 数据库分两种 有关联和没关联。（taobao算有关联）,一个项目一个database
+
+let DataStore = require('nedb');
+let db = new DataStore('happy.db');
+db.loadDatabase();
+// install: npm install nedb --save
+
+
+// create array for step 6.5
+let HappyTracker =[];
+
+//public folder
+app.use('/',express.static('public'));
+
+// step 6, create route on server, listening for a post request 听到信息时开始收集
+app.post('/happyData',(req,res)=>{
+    console.log("Received a POST request")
+    //catch the body send from client site 解析数据再制作成server中的json格式
+    // let happyObj = req.body;
+    // console.log(happyObj);
+    let currentDate=Date();
+    let happyObj_post = {
+        date: currentDate,
+        name: req.body.name,
+        Happy: req.body.Happiness  
+    }
+    //console.log(happyObj_post)
+  
+    // step 6.5 把数据(obj)装进array(coffeeTracker)
+    HappyTracker.push(happyObj_post);
+    console.log(HappyTracker);
+
+ 
+    //step 7 storage data
+    db.insert(HappyTracker);
+
+
+    // send msg back to client, response 
+    // client side : data send back - json (js: res= res=>{}
+    res.json({task:"success"});
+})
+
+
+app.listen(7000,()=>{
+    console.log("localhost:7000")
+})
+
+// // add route get information
+// app.get('/getAirQuality',(req,res)=>{
+//     let obj = { data : airTracker };
+//     res.json(obj);
+// })
---
+++ public/script.js
@@ -0,0 +1,63 @@
+window.addEventListener('load',function(){
+    console.log('hello from script.js')
+
+    let sentences = ["❤ You look great! ❤","❤ What a lovely day! ❤","❤ Super! ❤"];
+
+    
+    // step 1 add event listener
+    let button = document.getElementById('button-log');
+    button.addEventListener('click',()=>{
+
+        // step 2 grab input value
+        let HappyInfo = document.getElementById('happy-text').value;
+        console.log(HappyInfo);
+        let name = document.getElementById('happy-name').value;
+        console.log(name);
+
+
+   // make a fetch request - send to server
+        // step 3  creating obj
+        let happyObj_send = {  
+            "Happiness" : HappyInfo,
+            "name" : name
+        };
+        console.log(happyObj_send);
+
+        // setp 4, create json, stringify obj to json
+       let HappyObjJSON = JSON.stringify(happyObj_send);
+       console.log(HappyObjJSON)
+
+        //bg change img
+        
+
+        //step 5 fetch - post/send data to server, pass url and pass object
+        fetch('/happyData',{
+            //this is a post
+            method: 'POST',
+            // type of data 
+            headers:{'Content-Type':'application/json'},
+            // put the obj to this body
+            body: HappyObjJSON
+        })
+        //client side response
+        .then(res=>res.json())
+        .then(data=>{
+            console.log("Howay!")
+            console.log(data)
+
+            // random show sentence
+            let randomSentenceIndex = Math.floor(Math.random()*sentences.length);
+            let randomSentence = sentences[randomSentenceIndex]
+            console.log(randomSentence);
+    
+            document.getElementById('greet').textContent = randomSentence;
+           
+           // alert("Wonderful day!!");
+
+            
+        })
+        
+
+    })
+   
+})
